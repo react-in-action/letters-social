b2533d149cb35bad38c9a73fb9cc0274d3cb60ce
Mark Thomas <markthethomas@gmail.com>
2016-10-11 20:52:34 -0700

    fix build and static file serve

--- server/app.js
+++ server/app.js
@@ -1,60 +1,39 @@
 import bodyParser from 'body-parser';
 import jsonAPI from 'json-server';
 import compression from 'compression';
-import cookieParser from 'cookie-parser';
-import { join } from 'path';
+import { __PRODUCTION__ } from 'environs';
+import { join, resolve } from 'path';
 import cors from 'cors';
 import express from 'express';
 import helmet from 'helmet';
 import hpp from 'hpp';
 import logger from 'morgan';
 import responseTime from 'response-time';
-import { v4 as uuid } from 'node-uuid';
 
 // Le App
 const app = express();
 
 app.use(logger('dev'));
-
-app.use(helmet.xssFilter({
-  setOnOldIE: true,
-}));
-
+app.use(helmet.xssFilter({ setOnOldIE: true }));
 app.use(responseTime());
-
 app.use(helmet.frameguard());
-
 app.use(helmet.ieNoOpen());
-
 app.use(helmet.noSniff());
-app.use(helmet.hidePoweredBy({
-  setTo: 'coffee',
-}));
-
-// Should we really have another cookie?
-app.use(cookieParser(uuid.v4(), {}));
-
-// Parse it all
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
-
-// JSON
+app.use(helmet.hidePoweredBy({ setTo: 'react' }));
 app.use(bodyParser.json());
 
 // Forms and compression!
 app.use(compression());
-
-// Prevent parameter polution
 app.use(hpp());
-
 app.use(cors());
 
-app.options('*', cors());
+app.options(__PRODUCTION__ ? 'http://social.learnreactjs.io' : '*', cors());
 
 // ==== Serve the app ====
 app.use('/api', jsonAPI.router(join(__dirname, '..', '..', 'db', 'seed', 'db.json')));
-app.use('/', express.static(join(__dirname, '..')));
+app.use('/', express.static(resolve(__dirname, '..')));
+
+app.use('*', (req, res) => res.sendFile(resolve(__dirname, '..', 'index.html')));
 
 // ====ERROR-HANDLING===
 app.use((req, res, next) => {
@@ -67,11 +46,6 @@ app.use((err, req, res) => {
   console.error(err);
   return res.status(err.status || 500).json({
     message: err.message,
-    error: {
-      name: err.name,
-      stack: err.stack,
-      status: err.status,
-    },
   });
 });
 
