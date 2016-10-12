a39670adccc3881f2529c13b54805e6631bfa559
Mark Thomas <markthethomas@gmail.com>
2016-10-11 20:31:53 -0700

    refactor app core for use w/ router, history listener

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import { App } from './components/App';
-import { Home, SinglePost } from './containers';
+import { Home, SinglePost, Login } from './containers';
 import { Router, Route } from './components/router';
 import { history } from './history';
+import { firebase } from './backend';
 
 import './styles/styles.scss';
-
-const appContainer = document.getElementById('app');
-
-const initialState = {
-  location: window.location.pathname,
-};
 
 // Function that wraps ReactDOM.render and renders the app w/ current location
 const renderApp = (state) => {
@@ -21,22 +16,39 @@ const renderApp = (state) => {
       <Route path="" component={App}>
         <Route path="/" component={Home} />
         <Route path="/posts/:post" component={SinglePost} />
+        <Route path="/login" component={Login} />
       </Route>
     </Router>,
-    appContainer
+    document.getElementById('app'),
   );
 };
 
-// When things change, re-render the app
+const initialState = {
+  location: window.location.pathname,
+};
+
+// Render the app initially
+renderApp(initialState);
+
+// When there's a history change, re-render the app
 history.listen((location) => {
-  console.log(location);
+  const user = firebase.auth().currentUser;
   const newState = Object.assign(
-    {},
-    initialState,
-    { location: location.pathname }
-  );
+      initialState,
+      { location: user ? location.pathname : '/login' }
+    );
   renderApp(newState);
 });
 
-// Render the app w/ the current location
-renderApp(initialState);
+// When the user is
+firebase.auth().onAuthStateChanged((user) => {
+  const currentLocation = window.location.pathname;
+  if (currentLocation === '/') {
+    history.push(user ? window.location.pathname : '/login');
+  }
+
+  // Ensure user gets redirected after they log in
+  if (user && currentLocation === '/login') {
+    history.push('/');
+  }
+});
