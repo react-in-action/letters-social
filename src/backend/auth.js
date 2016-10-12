0ce7ff3ec05418d2f8b00242512bbc23ba1cbb8a
Mark Thomas <markthethomas@gmail.com>
2016-10-11 20:31:01 -0700

    feat: firebase backend

--- src/backend/auth.js
+++ src/backend/auth.js
@@ -1,6 +1,49 @@
-import firebase from 'firebase';
+import { history } from '../history';
+import { firebase } from './core';
+
+const google = new firebase.auth.GoogleAuthProvider();
+google.addScope('https://www.googleapis.com/auth/plus.login');
+
+const facebook = new firebase.auth.FacebookAuthProvider();
+facebook.addScope('public_profile');
 
 const github = new firebase.auth.GithubAuthProvider();
 github.addScope('user:email');
 
-export { github };
+const twitter = new firebase.auth.TwitterAuthProvider();
+
+
+export function signup(email, password) {
+  return new Promise((resolve, reject) => {
+    firebase.auth()
+    .createUserWithEmailAndPassword(email, password)
+    .catch(error => reject(error));
+  });
+}
+
+export function login(email, password) {
+  return new Promise((resolve, reject) => {
+    firebase.auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(() => history.push('/'))
+    .catch(error => reject(error));
+  });
+}
+
+export function logout() {
+  return new Promise((resolve, reject) => {
+    firebase.auth().signOut().then(() => {
+      history.push('/login');
+      resolve();
+    }, error => reject(error));
+  });
+}
+
+function loginWithProvider(provider) {
+  return firebase.auth().signInWithRedirect(provider);
+}
+
+export const loginWithGoogle = loginWithProvider.bind(null, google);
+export const loginWithTwitter = loginWithProvider.bind(null, twitter);
+export const loginWithGithub = loginWithProvider.bind(null, github);
+export const loginWithFacebook = loginWithProvider.bind(null, facebook);
---
+++ src/backend/core.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyDBosKGKi-BI9Z8vftAwkBRQlSDDNE8PvM',
+  authDomain: 'letters-social.firebaseapp.com',
+  databaseURL: 'https://letters-social.firebaseio.com',
+  storageBucket: 'letters-social.appspot.com',
+  messagingSenderId: '695497937993',
+};
+
+firebase.initializeApp(config);
+
+export { firebase };
