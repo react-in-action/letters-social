2ab8ba0c9204b0d5693bc1df625aa0a6cdec23a9
Mark Thomas <markthethomas@gmail.com>
2016-10-15 16:52:29 -0700

    update welcome stuff

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,101 +1,20 @@
-import React, { PropTypes } from 'react';
-import { Modal, Button } from 'react-bootstrap';
-import Letter from './Letter';
+import React from 'react';
+import { AutoAffix } from 'react-overlays';
 
-class Welcome extends React.Component {
-  static propTypes = {
-    onClose: PropTypes.func.isRequired,
-    show: PropTypes.bool.isRequired,
-  }
-  constructor(props) {
-    super(props);
-    this.showLetter = this.showLetter.bind(this);
-  }
-  state = {
-    showLetter: false,
-  }
-  showLetter() {
-    this.setState({
-      showLetter: true,
-    });
-  }
-  render() {
-    return (
-      <Modal show={this.props.show}>
-        <Modal.Header closeButton onClose={this.props.onClose}>
-          <Modal.Title>Welcome!</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>
-            If you're here, you're probably reading <em>React in Action</em> by, well,
-            me (<a href="https://github.com/markthethomas">@markthethomas</a>) 😀!
-            I hope you're enjoying learning about React so far! We'll be working
-            through each part of the project as we move through
-            it in the book, but you can find the complete project here for reference.
-            If you have any questions or thoughts, feel free to reach out to me
-            @markthethomas (
-            <a href="https://ifelse.io"><i className="fa fa-pencil"></i></a>
-            <a href="https://twitter.com/MarkTheThomas"><i className="fa fa-twitter"></i></a>
-            <a href="https://github.com/markthethomas"><i className="fa fa-github"></i></a>
-            ) pretty much anywhere.
-          </p>
-
-          <p>
-            - Mark
-          </p>
-
-
+export const Welcome = () => {
+  return (
+    <AutoAffix viewportOffsetTop={50} container={this}>
+      <div className="panel element-base">
+        <h3>Welcome!</h3>
+        <p>
+          If you're here, you're probably reading or heard about <a href="https://www.manning.com/books/react-in-action" target="_blank" rel="noopener noreferrer">React in Action</a> by, well,
+          me (@markthethomas everywhere). This app, Letters Social, is built with <a rel="noopener noreferrer" href="https://facebook.github.io/react/">React</a> what you'll be building in the book. It explores compoennts, React fundamentals, and If you have any questions
+          or thoughts, feel free to reach out to me <a href="https://twitter.com/markthethomas" target="_blank" rel="noopener noreferrer">@markthethomas</a> pretty much anywhere.
           <br />
-          <p>
-            How to get started:
-            <ul>
-              <li>
-                clone or download the project <a href="https://github.com/react-in-action/letters-social">here</a>
-              </li>
-              <li>
-                install project dependencies using <code>npm install</code>
-              </li>
-              <li>
-                read welcome letter from Letters (see below)
-              </li>
-              <li>
-                read along in React in Action
-              </li>
-            </ul>
-          </p>
-
           <br />
-          {
-            this.state.showLetter ?
-              null
-              :
-              <Button
-                block
-                bsStyle="success"
-                onClick={this.showLetter}
-              > Read welcome letter <i className="fa fa-check"></i>
-              </Button>
-          }
-
-          <br />
-          <Letter show={this.state.showLetter} />
-        </Modal.Body>
-        <Modal.Footer>
-          {
-            this.state.showLetter ?
-              <Button
-                block
-                onClick={this.props.onClose}
-              >
-                Get started
-              </Button>
-              :
-              null
-          }
-        </Modal.Footer>
-      </Modal>
-    );
-  }
-}
-
-export default Welcome;
+          - Mark
+        </p>
+      </div>
+    </AutoAffix>
+  );
+};
