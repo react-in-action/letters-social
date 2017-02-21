16d8f45d260b226470fb4dfc7f1d4a4db6bc6fe4
Mark Thomas <markthethomas@gmail.com>
2017-02-21 02:06:51 -0800

    fix tests

--- src/containers/Home.test.js
+++ src/containers/Home.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
 const sinon = require('sinon');
 import configureStore from '../store/configureStore';
