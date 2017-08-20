85bdcb06d8978f66551ec57ec57282b85d0dfdd7
Mark Thomas <markthethomas@gmail.com>
2017-08-20 14:01:30 -0500

    Minor server cleanup

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 // Normally we wouldn't use this babel-register hook in a production situation;
-// a build step or something else would replace it
+// a build step or something else could replace it
 require('babel-register');
+
+// All this is express boilerplate
 const bodyParser = require('body-parser');
 const jsonAPI = require('json-server');
 const compression = require('compression');
@@ -12,20 +14,23 @@ const helmet = require('helmet');
 const hpp = require('hpp');
 const logger = require('morgan');
 const responseTime = require('response-time');
+
+// Modules explicitly related to React & SSR
 const { renderToString } = require('react-dom/server');
 const React = require('react');
 const { match, RouterContext } = require('react-router');
 const { Provider } = require('react-redux');
 
+// Our modules
 const { routes } = require('../src/routes');
 const configureStore = require('../src/store/configureStore').default;
 const initialReduxState = require('../src/constants/initialState');
 
-// Le App
+// Create the express app
 const app = express();
 
+// Add some boilerplate middlware
 app.use(logger('dev'));
-
 app.use(helmet.xssFilter({ setOnOldIE: true }));
 app.use(responseTime());
 app.use(helmet.frameguard());
@@ -33,27 +38,26 @@ app.use(helmet.ieNoOpen());
 app.use(helmet.noSniff());
 app.use(helmet.hidePoweredBy({ setTo: 'react' }));
 app.use(bodyParser.json());
-
-// Forms and compression!
 app.use(compression());
 app.use(hpp());
 app.use(cors());
 
+// Route handlers
 app.options(__PRODUCTION__ ? 'http://social.learnreactjs.io' : '*', cors());
-
-// ==== Serve the app ====
 app.use(
     '/api',
     jsonAPI.router(resolve(__dirname, '..', 'db', 'seed', 'db.json'))
 );
-
 app.use('/static', express.static(resolve(__dirname, '..', 'static')));
+// SSR-related functionality
 app.use('*', (req, res) => {
+    // Use React Router to match
     match(
         { routes: routes, location: req.originalUrl },
         (err, redirect, props) => {
-            // Configure the Redux store
+            // Configure the Redux store so we can use it during SSR
             const store = configureStore(initialReduxState);
+            // HTML to send down to the browser
             const appHtml = renderToString(
                 React.createElement(
                     Provider,
@@ -61,6 +65,7 @@ app.use('*', (req, res) => {
                     React.createElement(RouterContext, props, props.children)
                 )
             );
+            // Use am ES2015+ string literal to interpolate our app HTML into the page
             const html = `
             <!doctype html public="storage">
             <html>
@@ -79,24 +84,22 @@ app.use('*', (req, res) => {
                 <script id="intialState">window.__INTIIAL_STATE__ = ${JSON.stringify(
                     store.getState()
                 )}</script>
-                <script src="/static/bundle.js" type='text/javascript'></script>
+                <script async defer src="/static/bundle.js" type='text/javascript'></script>
                 </body>
             </html>
         `.trim();
-            // console.log(store.getState());
             res.setHeader('Content-type', 'text/html');
             res.send(html).end();
         }
     );
 });
 
-// ====ERROR-HANDLING===
+// Error handling routes
 app.use((req, res, next) => {
     const err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
-
 app.use((err, req, res) => {
     console.error(err);
     return res.status(err.status || 500).json({
