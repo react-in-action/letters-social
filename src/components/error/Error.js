c5ccb2c3064709e388fecca118b1a08b5ef8d293
Mark Thomas <markthethomas@gmail.com>
2017-09-04 15:55:35 -0700

    Add error component

--- src/components/error/Error.js
+++ src/components/error/Error.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
+export function renderErrorMessage(err) {
+    return (
+        err.stack ||
+        err.stacktrace ||
+        (console.trace ? console.trace() : 'no error stack available')
+    );
+}
 const ErrorMessage = ({ error }) => {
     return (
         <div className="error">
             <h2 className="message">Something went wrong</h2>
             <p>We're on it!</p>
-            <code>{error}</code>
+            <code>{renderErrorMessage(error)}</code>
+            <button className="block">
+                <a
+                    href="https://github.com/react-in-action/letters-social/issues/new"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                >
+                    Open an issue on Github
+                </a>
+            </button>
         </div>
     );
 };
